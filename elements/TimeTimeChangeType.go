10aabb473c90b62a93dc9cd904d7e56886b655bc
Paul Orzel <pb.orzel@proton.me>
2026-03-02 16:17:08 +0100

    feat: replace default time with custom parsed wrapper

--- elements/TimeTimeChangeType.go
+++ elements/TimeTimeChangeType.go
@@ -2,13 +2,36 @@ package elements
 
 // The Time element describes the time when the time changes between standard time and daylight saving time.
 // https://learn.microsoft.com/en-us/exchange/client-developer/web-service-reference/time-timechangetype
-import "time"
+import (
+	"encoding/xml"
+	"time"
+)
 
-import "encoding/xml"
+type ClockTime struct {
+	Time time.Time `xml:",chardata"`
+}
+
+func (c *ClockTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var s string
+
+	if err := d.DecodeElement(&s, &start); err != nil {
+		return err
+	}
+
+	// Parse using the TimeOnly layout: "15:04:05"
+	t, err := time.Parse(time.TimeOnly, s)
+	if err != nil {
+		return err
+	}
+
+	*c = ClockTime{Time: t}
+
+	return nil
+}
 
 type TimeTimeChangeType struct {
 	XMLName xml.Name
-	TEXT    time.Time `xml:",chardata"`
+	TEXT    ClockTime `xml:",chardata"`
 }
 
 func (T *TimeTimeChangeType) SetForMarshal() {
