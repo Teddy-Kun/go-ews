667d0071d7cb8cad40807b247fc5106c6421fb46
Paul Orzel <pb.orzel@proton.me>
2026-04-02 13:20:49 +0200

    fix: ChangesItems in SyncFolderItemsResponseMessage contains slices

--- elements/ChangesItems.go
+++ elements/ChangesItems.go
@@ -8,13 +8,13 @@ type ChangesItems struct {
 	XMLName xml.Name
 
 	// The Create element identifies a single item to create in the local client store.
-	Create *CreateItemSync `xml:"Create"`
+	Create []CreateItemSync `xml:"Create"`
 	// The Delete element identifies a single item to delete in the local client store.
-	Delete *DeleteItemSync `xml:"Delete"`
+	Delete []DeleteItemSync `xml:"Delete"`
 	// The ReadFlagChange element is returned in SyncFolderItems operation responses when an item has been read. This property is read-only.
-	ReadFlagChange *ReadFlagChange `xml:"ReadFlagChange"`
+	ReadFlagChange []ReadFlagChange `xml:"ReadFlagChange"`
 	// The Update element identifies a single item to update in the local client store.
-	Update *UpdateItemSync `xml:"Update"`
+	Update []UpdateItemSync `xml:"Update"`
 }
 
 func (C *ChangesItems) SetForMarshal() {
