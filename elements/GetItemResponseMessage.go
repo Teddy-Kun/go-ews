582ead3c9295c289fe0ed6af320c9d1e9eeaab74
Paul Orzel <pb.orzel@proton.me>
2026-04-07 10:40:11 +0200

    Revert "fix: replace another pointer with a slice"
    This reverts commit e2401b98d7ab3d77f9fafaab5d20e10d387fcfe7.

--- elements/GetItemResponseMessage.go
+++ elements/GetItemResponseMessage.go
@@ -10,7 +10,7 @@ type GetItemResponseMessage struct {
 	// The DescriptiveLinkKey element is currently unused and is reserved for future use. It contains a value of 0.
 	DescriptiveLinkKey *DescriptiveLinkKey `xml:"DescriptiveLinkKey"`
 	// The Items element contains an array of items.
-	Items []Items `xml:"Items"`
+	Items *Items `xml:"Items"`
 	// The MessageText element provides a text description of the status of the response.
 	MessageText *MessageText `xml:"MessageText"`
 	// The MessageXml element provides additional error response information.
