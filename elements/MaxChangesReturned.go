16081076312a15262cc158e02f2a8b265f85449e
Paul Orzel <pb.orzel@proton.me>
2026-04-07 09:22:38 +0200

    fix: use m namespace for MaxChangesReturned
    This might break email support, but I don't care, I need calendar only

--- elements/MaxChangesReturned.go
+++ elements/MaxChangesReturned.go
@@ -10,7 +10,7 @@ type MaxChangesReturned struct {
 }
 
 func (M *MaxChangesReturned) SetForMarshal() {
-	M.XMLName.Local = "t:MaxChangesReturned"
+	M.XMLName.Local = "m:MaxChangesReturned"
 }
 
 func (M *MaxChangesReturned) GetSchema() *Schema {
